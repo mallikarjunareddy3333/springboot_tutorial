3305406385d5ed714e3350be43762928cdcc9cb4
Mallikarjuna Reddy Giddam <mallikarjunareddy3333@gmail.com>
2019-01-18 06:54:53 +0530

    - Added datasource info & dependencies

--- src/main/java/com/springboot/tutorial/entity/Product.java
+++ src/main/java/com/springboot/tutorial/entity/Product.java
@@ -1,8 +1,21 @@
 package com.springboot.tutorial.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCT")
 public class Product {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String id;
+
+	@Column(name = "name", nullable = false, length = 100)
 	private String name;
 
 	public String getId() {
